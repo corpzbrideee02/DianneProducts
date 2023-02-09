9b0d6fbcac0777726a39c13f9e1b92ec33072c6c
Dianne <corpuzdianne0302@gmail.com>
2023-02-09 13:03:21 -0500

    feat: add pageNotfound, add page routing HashRouter

--- src/index.js
+++ src/index.js
@@ -3,28 +3,34 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import { getProducts } from './components/actions/actions'
-import App from './App'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider} from '@mui/material/styles';
-import {theme} from './components/theme';
+import { Routes, Route, HashRouter } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './components/theme';
 import Home from './components/Home';
 import Cart from './components//Cart';
 import Products from './components/Products';
-
+import PageNotFound from '../src/404.png';
+import Container from '@mui/material/Container';
 //Dispatch the getProducts() before our root component renders
 store.dispatch(getProducts())
 
 ReactDOM.render(
-    <Provider store={ store }>
-      <BrowserRouter>
+  <Provider store={store}>
+    <HashRouter>
       <ThemeProvider theme={theme}>
-      <Routes>
-        
-      <Route exact path="" element={<Home />} />
-      <Route exact path='/products' element={<Products />} />
-       <Route exact path='/Cart' element={<Cart />} />
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route
+              path="*"
+              element={
+                  <Container maxWidth="sm">
+                   <img src={PageNotFound} width="500" alt="Page Not found"/>
+                </Container>
+              }/>
+        </Routes>
       </ThemeProvider>
-      </BrowserRouter>
-    </Provider>, document.getElementById('root')
+    </HashRouter>
+  </Provider>, document.getElementById('root')
 );
